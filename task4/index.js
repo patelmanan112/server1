6b0e4d75a1d9daf89033cfce1961d49a73f55b22
patelmanan112 <manan.patel.cg@gmail.com>
2026-02-17 15:38:54 +0530

    adding new two method

--- task4/index.js
+++ task4/index.js
@@ -66,6 +66,32 @@ app.post('/user' , (req , res)=>{
     users.push(user);
     res.status(201).json({"added": user})
 })
+
+app.delete("/user/:uid" , (req , res)=>{
+    const uid = Number(req.params.uid);
+    const user = users.findIndex(u=> u.uid ===  uid);
+    users.splice(user , 1);
+    res.status(201).json(users);
+})
+
+app.patch("/users/:uid" , (req , res)=>{
+
+
+    const userId = Number(req.params.uid);
+    console.log(userId);
+    
+    const  user = users.find(u => u.uid === userId);
+    console.log(user);
+    if(!user){
+        return res.status(404).json({"message": "User does not found"});
+    }
+    if(req.body.att) user.att = req.body.att;
+    if(req.body.total_sub) user.total_sub = req.body.total_sub;
+     if(req.body.bonus) user.bonus = req.body.bonus;
+      if(req.body.name) user.bonus = req.body.name;
+
+    res.status(200).send("User updated" , user);
+})
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
